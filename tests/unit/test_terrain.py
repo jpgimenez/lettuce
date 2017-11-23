521fa57acc802fb15e6db453228b1cf9dce65fb9
Juan Pablo Giménez <jgimenez@velocitypartners.net>
2017-11-22 22:32:11 -0300

    switch to mox3

--- tests/unit/test_terrain.py
+++ tests/unit/test_terrain.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import sys
-from mox import Mox
+from mox3.mox import Mox
 from nose.tools import assert_equals
 
 from lettuce import step
